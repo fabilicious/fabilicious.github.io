c45b66d8a65aa693882c3398498cb7332ea40f4d
beramos <bram.de.jaegher@gmail.com>
2017-12-25 23:07:37 +0100

    functional recipe filter

--- app/filters.js
+++ app/filters.js
@@ -1,6 +1,22 @@
 angular.module('app.filters', [])
-.filter('reverse', function() {
-      return function(input, uppercase) {
+.filter('fuzzyFilter', function() {
+    var options = {
+        shouldSort: true,
+        tokenize: true,
+        matchAllTokens: true,
+        threshold: 0.6,
+        location: 0,
+        distance: 100,
+        maxPatternLength: 32,
+        minMatchCharLength: 1,
+        keys: [
+            "title",
+            "author.firstName"
+        ]
+    };
+    var fuse = new Fuse(input, options); // "list" is the item array  
+    return fuse.search("Remy");
+      /*return function(input, uppercase) {
         input = input || '';
         var out = '';
         for (var i = 0; i < input.length; i++) {
@@ -11,5 +27,5 @@ angular.module('app.filters', [])
           out = out.toUpperCase();
         }
         return out;
-      };
+      };*/
 });
